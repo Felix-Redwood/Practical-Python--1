27bb1b018b46fc67c63747753e5e23e90d438084
Felix-Redwood <Felix@grahamredwood.com>
2020-04-01 11:38:32 +0000

    added {{page_title}}

--- run.py
+++ run.py
@@ -9,15 +9,15 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html") #when we navigate to about.html, the template will be returned
+    return render_template("about.html", page_title="About") #when we navigate to about.html, the template will be returned
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html") #when we navigate to contact.html, the template will be returned
+    return render_template("contact.html", page_title="Contact") #when we navigate to contact.html, the template will be returned
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html") #when we navigate to careers.html, the template will be returned
+    return render_template("careers.html", page_title="Careers") #when we navigate to careers.html, the template will be returned
 
 if __name__ == "__main__": #__main__ is the name of the default module in python
     app.run(host=os.environ.get("IP"),
